685d92f6d635ec383b4b7561ec9b3693007950e2
Micaela O'Herron <micaela.oherron@gmail.com>
2019-01-14 12:19:09 -0500

    testing deployment

--- server/index.js
+++ server/index.js
@@ -22,9 +22,8 @@ app.use((err, req, res, next) => {
   res.status(err.status || 500).send(err.message || "Internal server error");
 });
 
-app.get("*", (req, res) => {
-  // res.send(path.join(__dirname, "../public"));
-  res.sendFile(path.join(__dirname, "../public/index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../public/index.html"));
+// });
 
 module.exports = app;
