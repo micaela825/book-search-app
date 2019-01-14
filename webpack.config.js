7f5081dd97f3155accf8cb93205085c3b876b418
Micaela O'Herron <micaela.oherron@gmail.com>
2019-01-14 12:32:48 -0500

    testing deployment

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,55 @@
-"use strict";
+// "use strict";
 
-const { resolve } = require("path");
+// const { resolve } = require("path");
+
+// module.exports = {
+//   entry: ["babel-polyfill", "./app/main"],
+//   output: {
+//     path: __dirname,
+//     filename: "./public/bundle.js"
+//   },
+//   mode: "development",
+//   context: __dirname,
+//   devtool: "source-map",
+//   resolve: {
+//     extensions: [".js", ".jsx"]
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: /jsx?$/,
+//         include: resolve(__dirname, "./app"),
+//         loader: "babel-loader"
+//       },
+//       {
+//         test: /\.css$/,
+//         use: ["style-loader", "css-loader"]
+//       }
+//     ]
+//   }
+// };
+
+const isDev = process.env.NODE_ENV === "development";
 
 module.exports = {
-  entry: ["babel-polyfill", "./app/main"],
+  entry: [
+    "@babel/polyfill", // enables async-await
+    "./app/main.js"
+  ],
   output: {
     path: __dirname,
     filename: "./public/bundle.js"
   },
-  mode: "development",
-  context: __dirname,
-  devtool: "source-map",
   resolve: {
     extensions: [".js", ".jsx"]
   },
+  devtool: "source-map",
   module: {
     rules: [
       {
-        test: /jsx?$/,
-        include: resolve(__dirname, "./app"),
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
         loader: "babel-loader"
-      },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"]
       }
     ]
   }
